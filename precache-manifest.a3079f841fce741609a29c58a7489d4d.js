14e60dc7d3c6bb8e21b181a83350ba39e6d381df
fortena <arnigeir@gmail.com>
2019-07-04 23:20:35 +0200

    Updates

---
+++ precache-manifest.a3079f841fce741609a29c58a7489d4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce52a6e6a3c7f83f2810a63b0bf8095",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27db7bea2aced8a5b4a2",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d4a09f328e710665d980",
+    "url": "/static/js/2.b71afe1e.chunk.js"
+  },
+  {
+    "revision": "27db7bea2aced8a5b4a2",
+    "url": "/static/js/main.52f6c674.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
